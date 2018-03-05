ea7886f09c5bbdcba6174a38a0a9ce7fc2290e7a
christianpetri <christianpetri@users.noreply.github.com>
2018-03-05 11:22:14 +0100

    Next Button

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,15 @@
 /*
 $(document).ready(function () {
-    $searchQuery = "dark";
-    $url = 'https://api.themoviedb.org/3/search/movie?api_key=84b8bbc00a5c8c683ef60c5709687388&language=en-US&query=' + $searchQuery + '&page=1&include_adult=false';
-    $.get($url, function (data) {
-        var movies = data.results;
-        movies.forEach(function (movie) {
-            $('<li>')
-                .appendTo('#result')
-                .text(movie.title)
-                .append(
-                    $('<button onclick="getMovieDetails('+movie.id +')">')
-                        .text("Get Details")
-                )
-        })
-    })
+
 });
 */
-function  getMoviesQuery() {
+
+function  getMoviesQuery($currentPage) {
     $searchQuery = $('#searchQueryInput').val();
+    $currentPage = parseInt($currentPage, 10)+ 1;
+    $apiKey = "84b8bbc00a5c8c683ef60c5709687388";
     $("#result").empty();
-    $url = 'https://api.themoviedb.org/3/search/movie?api_key=84b8bbc00a5c8c683ef60c5709687388&language=en-US&query=' + $searchQuery + '&page=1&include_adult=false';
+    $url = 'https://api.themoviedb.org/3/search/movie?api_key='+ $apiKey +'&language=en-US&query=' + $searchQuery + '&page='+$currentPage+'&include_adult=false';
     $.get($url, function (data) {
         var movies = data.results;
         movies.forEach(function (movie) {
@@ -27,13 +17,18 @@ function  getMoviesQuery() {
                 .appendTo('#result')
                 .text(movie.title)
                 .append(
-                    $(' <button onclick="getMovieDetails('+movie.id +')">')
+                    $(' <button onclick="getMovieDetails(' + movie.id + ')">')
                         .text("Get Details")
                 )
-        })
+        });
+        $("#moviePageNavigation").empty();
+        $('<div>')
+            .appendTo('#moviePageNavigation')
+            .text("Current Page number " + $currentPage)
+            .html("<button onclick=\"getMoviesQuery('" + $currentPage + "')\">Next Button</button>" )
     })
 }
-/*
+
 function getMovieDetails($movieID) {
     $url    = 'https://api.themoviedb.org/3/movie/' + $movieID +'?api_key=84b8bbc00a5c8c683ef60c5709687388';
     //$url="https://api.themoviedb.org/3/movie/4911?api_key=84b8bbc00a5c8c683ef60c5709687388";
@@ -41,29 +36,15 @@ function getMovieDetails($movieID) {
         var movie = data;
         $("#resultDetail").empty();
         $('<li>')
-                .appendTo('#resultDetail')
-                .text(movie.overview)
-                .append(
-                    $("<br><img src='https://image.tmdb.org/t/p/w500"+ movie.backdrop_path + "' style='height:100px'/>")
-                )
-    })
-}
-*/
-function getMovieDetails($movieID) {
-    $url    = 'https://api.themoviedb.org/3/movie/' + $movieID +'?api_key=84b8bbc00a5c8c683ef60c5709687388';
-    //$url="https://api.themoviedb.org/3/movie/4911?api_key=84b8bbc00a5c8c683ef60c5709687388";
-    $.get($url, function (data) {
-        var movie = data;
-        $("#resultDetail").empty();
-        $('<li>')
-
-
+            .appendTo('#resultDetail')
             .text(movie.overview)
             .append(
                 $("<br><img src='https://image.tmdb.org/t/p/w500"+ movie.backdrop_path + "' style='height:100px'/>")
             )
     })
 }
+
+
 /*
 function httpGetAsync(theUrl) {
     var xmlHttp = new XMLHttpRequest();
