079f2c08ac8d81df3d9833d262735152eee7dca3
christianpetri <christianpetri@users.noreply.github.com>
2018-03-05 14:24:12 +0100

    pagination  added

--- src/app/app.js
+++ src/app/app.js
@@ -1,32 +1,54 @@
-/*
+
 $(document).ready(function () {
+    $("#searchQueryInput").keyup(function(event) {
+        if (event.keyCode === 13)  //13 --> pressed enter key
+            $('#seachQueryButton').click();
+    });
 
 });
-*/
+
+
 
 function  getMoviesQuery($currentPage) {
     $searchQuery = $('#searchQueryInput').val();
-    $currentPage = parseInt($currentPage, 10)+ 1;
+
     $apiKey = "84b8bbc00a5c8c683ef60c5709687388";
     $("#result").empty();
+    $("#resultDetail").empty();
     $url = 'https://api.themoviedb.org/3/search/movie?api_key='+ $apiKey +'&language=en-US&query=' + $searchQuery + '&page='+$currentPage+'&include_adult=false';
     $.get($url, function (data) {
         var movies = data.results;
+        $movie = data;
         movies.forEach(function (movie) {
             $('<li>')
                 .appendTo('#result')
-                .text(movie.title)
+                .text(movie.title +" ")
                 .append(
                     $(' <button onclick="getMovieDetails(' + movie.id + ')">')
                         .text("Get Details")
                 )
-        });
+        })
         $("#moviePageNavigation").empty();
+        $moveOnePageForward = $currentPage;
+        $moveOnePageBack = $currentPage;
+        if($currentPage < $movie.total_pages)
+            $moveOnePageForward =   parseInt($currentPage, 10)  +   1;
+        if($currentPage > 1)
+            $moveOnePageBack    =   parseInt($currentPage, 10)  -   1;
+
         $('<div>')
             .appendTo('#moviePageNavigation')
-            .text("Current Page number " + $currentPage)
-            .html("<button onclick=\"getMoviesQuery('" + $currentPage + "')\">Next Button</button>" )
-    })
+            .text("Page " + $currentPage + " of " + $movie.total_pages +" ")
+
+            .append(
+                $("<button onclick=\"getMoviesQuery('1')\">First Page</button>" ),
+                $("<button onclick=\"getMoviesQuery('" + $moveOnePageBack + "')\">previous page</button>" ),
+                $("<button onclick=\"getMoviesQuery('" + $moveOnePageForward + "')\">next page</button>" ),
+                $("<button onclick=\"getMoviesQuery('" + $movie.total_pages + "')\">last page</button>" )
+
+            )
+
+    });
 }
 
 function getMovieDetails($movieID) {
@@ -34,13 +56,19 @@ function getMovieDetails($movieID) {
     //$url="https://api.themoviedb.org/3/movie/4911?api_key=84b8bbc00a5c8c683ef60c5709687388";
     $.get($url, function (data) {
         var movie = data;
+        $imgae = "";
         $("#resultDetail").empty();
-        $('<li>')
+        if(movie.backdrop_path !== null) {
+            $image = "<br><img src='https://image.tmdb.org/t/p/w500" + movie.backdrop_path + "' style='width:100%'/>";
+        } else{
+            $image = "<br> No image available";
+        }
+
+        $('<div>')
             .appendTo('#resultDetail')
-            .text(movie.overview)
-            .append(
-                $("<br><img src='https://image.tmdb.org/t/p/w500"+ movie.backdrop_path + "' style='height:100px'/>")
-            )
+            .html("<h1>" + movie.title + " <small>("+movie.release_date.substring(0, 4)+")</small></h1>" + movie.overview)
+            .append( "<br>" + $image )
+
     })
 }
 
