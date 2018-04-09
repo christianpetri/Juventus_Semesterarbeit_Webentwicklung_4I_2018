3be51f74e61ae5f77968de654662de9332d16b73
christianpetri <christianpetri@hotmail.com>
2018-04-09 16:32:53 +0200

    removed some html an added the it as "JavaScript"

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,7 +7,6 @@ import {apiKey} from './constants';
 import '../css/index.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../img/logo_tmdb.png';
-//import model from "./movie-model";
 import {DefaultMovieModel} from "./movie-model";
 import {Movie} from "./Movie";
 import any = jasmine.any;
@@ -17,42 +16,29 @@ let model = new DefaultMovieModel();
 
 route('/', function () {
     $('#content').empty();
-    $('<div>').appendTo('#content').html(
-        '<h1>Search Movie Database</h1>' +
-        '<input type="text" id="searchQueryInput"/>' +
-        '<br>'
-    );
+    $('<h1>').appendTo('#content').text('Search Movie Database');
+    $('<input type="text">').appendTo('#content').attr('id', 'searchQueryInput');
     $('<button>').appendTo('#content')
         .addClass('btn btn-primary btn-sm')
-        .text('Search Movie Database')
+        .text(' Search Movie Database ')
         .on('click', () => {doSearch()});
-    $('<div>').appendTo('#content').html(
-    '<div id="moviePageNavigation"></div>' +
-    '<div class="row">' +
-    '<div id="resultMovieList" class="col-sm-5"></div>' +
-    '<div id="resultMovieListDetail" class="col-sm-7"></div>' +
-    '</div>'
-);
+    //'<div id="moviePageNavigation"></div>'
+    standardMoiveBody();
 });
+
 route('/top', function () {
 	$('#content').empty();
-	$('<div>').appendTo('#content').html(
-		'<h1>Top Rated Movies</h1>' +
-		'<br>' +
-		'<div id="moviePageNavigation"></div>' +
-		'<div class="row">' +
-		'<div id="resultMovieList" class="col-sm-5"></div>' +
-		'<div id="resultMovieListDetail" class="col-sm-7"></div>' +
-		'</div>'
-	);
+	//'<div id="moviePageNavigation"></div>'
+    $('<h1>').appendTo('#content').text('Top Rated Movies');
+    standardMoiveBody();
 	getTopRatedMovies();
 });
 
-////https://api.themoviedb.org/3/discover/movie?api_key=84b8bbc00a5c8c683ef60c5709687388&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=12
+//https://api.themoviedb.org/3/discover/movie?api_key=84b8bbc00a5c8c683ef60c5709687388&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=12
 route('/topgenre', function () {
 	$('#content').empty();
-	$('<h1>').appendTo('#content').text('Top ... by genres');
-	$('<div>').appendTo('#content').attr('id' , 'genres').addClass('col-sm-2');
+	$('<h1>').appendTo('#content').text('Top Rated Movies by Genres');
+	$('<div>').appendTo('#content').attr('id' , 'genres').addClass('col-sm-2 list-group');
 	$('<div>').appendTo('#content').attr('id' , 'resultMovieList').addClass('col-sm-4');
 	$('<div>').appendTo('#content').attr('id' , 'resultMovieListDetail').addClass('col-sm-5');
     $('#genres,#resultMovieList,#resultMovieListDetail').wrapAll('<div>').addClass('row');
@@ -63,6 +49,9 @@ route('/topgenre', function () {
 			const genreID : number = genre[i].id;
 			$('<div>').appendTo('#genres')
 				.html(genre[i].name)
+                .addClass('list-group-item')
+                .css('margin-right','10px')
+                .css('margin-left', '15px')
 				.on( 'click', () => { doSearchForGenres( {genreID: genreID } )});
 		}
 	});
@@ -72,12 +61,9 @@ route('/topgenre', function () {
 route('upcoming', function () {
     $('#content').empty();
     $('<h1>').appendTo('#content').text('Upcoming Movies');
-    $('<div>').appendTo('#content').attr('id' , 'resultMovieList').addClass('col-sm-5');
-    $('<div>').appendTo('#content').attr('id' , 'resultMovieListDetail').addClass('col-sm-7');
-    $('#resultMovieList,#resultMovieListDetail').wrapAll('<div>').addClass('row');
+    standardMoiveBody();
     getUpcomingMovies();
 });
-
 
 $(model).on('modelchange',() => {
 	renderMovies();
@@ -91,7 +77,10 @@ function renderMovies() {
 			.appendTo('#resultMovieList')
             .add('col-sm-5')
 			.text(movie.title)
-            .on('click', () => { showDetails( model.getMovie(movie.id)) });
+            .on('click', () => { showDetails( model.getMovie(movie.id)) })
+            .addClass('list-group-item')
+            .css('margin-left', '15px')
+            .css('margin-right','20px');
 	}
 }
 
@@ -115,18 +104,20 @@ function doSearchForGenres(parameters: { genreID: any }) {
 function showDetails(movie : Movie) {
     const resultMovieListDetail = $('#resultMovieListDetail');
     resultMovieListDetail.html('');
-    let image = '';
-    if (movie.backdrop_path !== null) {
-        image = '<br><img src=\'https://image.tmdb.org/t/p/w500' + movie.backdrop_path + '\' style=\'width:100%\'/>';
-    } else {
-        image = '<br> No image available';
-    }
-
-    $('<div>')
+    $('<h1>')
         .appendTo('#resultMovieListDetail')
-        .html('<h1 class="media-heading">' + movie.title + ' <small>(' + movie.release_date.substring(0, 4) + ')</small></h1>' + movie.overview)
-        .append('<br>' + image);
-
+        .text(movie.title)
+        .append($('<small>')
+        .text( ' ' +movie.release_date.substring(0, 4))).addClass('media-heading');
+    $('<div>').appendTo('#resultMovieListDetail').text(movie.overview);
+    if (movie.backdrop_path !== null) {
+        $('<img>', {
+            src: 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path,
+            width: '100%'
+        }).appendTo('#resultMovieListDetail').addClass('.img-fluid').css('padding-top', '10px');
+    } else{
+        $('<div>').appendTo('#resultMovieListDetail').text('No Image found').css('padding-top', '10px');
+    }
 }
 //https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1
 function getTopRatedMovies() {
@@ -149,6 +140,12 @@ function getUpcomingMovies() {
     const currentPage = 1;
     const url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=' + apiKey + '&language=en-US&&page=' + currentPage;
     addMovies(url);
+}
+
+function standardMoiveBody(){
+    $('<div>').appendTo('#content').attr('id' , 'resultMovieList').addClass('col-sm-5').addClass('list-group');
+    $('<div>').appendTo('#content').attr('id' , 'resultMovieListDetail').addClass('col-sm-7');
+    $('#resultMovieList,#resultMovieListDetail').wrapAll('<div>').addClass('row');
 }
 
 route.stop(); // clear all the old router callbacks
